21b72d43fb867ac7f89f652e6fdfe8d8851a0386
Dilip Kumar Patnaik Sadasivani <dsadasivani@gmail.com>
2022-04-23 23:53:11 +0530

    quiz app wrap up

--- lib/answer.dart
+++ lib/answer.dart
@@ -8,8 +8,18 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
+      child: ElevatedButton(
+        //***NOTES: In order to style button, use below property***
+        //Option 1:
+        // style: ButtonStyle(
+        //   backgroundColor: MaterialStateProperty.all(Colors.blue),
+        //   foregroundColor: MaterialStateProperty.all(Colors.white),
+        // ),
+        //Option 2:
+        // style: ElevatedButton.styleFrom(
+        //   primary: Colors.blue,
+        //   onPrimary: Colors.white,
+        // ),
         child: Text(answerText),
         onPressed: handler,
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,48 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   final _questions = const [
     {
       'questionText': 'What\'s your favourite color',
-      'answers': ['Black', 'Blue', 'Yellow', 'White'],
+      'answers': [
+        {'text': 'White', 'score': 1},
+        {'text': 'Blue', 'score': 2},
+        {'text': 'Yellow', 'score': 3},
+        {'text': 'Black', 'score': 4},
+      ],
     },
     {
       'questionText': 'What\'s your favourite animal',
-      'answers': ['Horse', 'Hippo', 'Tiger', 'Cheetah'],
+      'answers': [
+        {'text': 'parrot', 'score': 1},
+        {'text': 'Hippo', 'score': 2},
+        {'text': 'Cheetah', 'score': 3},
+        {'text': 'Fox', 'score': 4},
+      ],
     },
     {
       'questionText': 'What\'s your favourite Hero',
-      'answers': ['Yash', 'AA', 'NTR', 'Ram Charan'],
+      'answers': [
+        {'text': 'Yash', 'score': 1},
+        {'text': 'AA', 'score': 2},
+        {'text': 'NTR', 'score': 3},
+        {'text': 'Ram Charan', 'score': 4},
+      ],
     },
   ];
-  void _answerQuestions() {
+  void _answerQuestions(int score) {
     setState(() {
       _questionIndex = _questionIndex + 1;
+      _totalScore += score;
     });
     if (_questionIndex < _questions.length) {
       print('more questions');
@@ -50,14 +75,16 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text(
+            'Behavioural Quiz App',
+          ),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
                 questions: _questions,
                 questionIndex: _questionIndex,
                 answerQuestion: _answerQuestions)
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
